51ea359cc2c130e44420b19647bba627d0d3bda1
vikas <rawatvikas689@gmail.com>
2024-02-21 19:34:49 +0530

    container config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Yangs-Kitchen-clone/',
+  // base: '/Yangs-Kitchen-clone/',
 })
