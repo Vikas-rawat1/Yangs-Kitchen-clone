7cece2aa2b0b131f9f340865c6646c3318d3d481
vikas <rawatvikas689@gmail.com>
2024-03-02 17:46:37 +0530

    container config

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import DatePicker from 'react-datepicker';
+import DatePicker from "react-datepicker";
 function Reservation() {
+
   return (
     <>
       <div className="relative">
@@ -13,7 +14,6 @@ function Reservation() {
             alt="Background"
           />
         </div>
-
         <div className="absolute top-0 left-0 right-0 flex justify-center mt-10">
           <img
             className="h-8 object-cover"
@@ -22,7 +22,6 @@ function Reservation() {
           />
           <p className="text-white font-bold items-center flex">Reservation</p>
         </div>
-
         <div className="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2   w-4/5 h-72  rounded-md bg-white border-2">
           <h1 className="text-center font-semibold text-2xl mt-4">
             Yang’s Kitchen
@@ -42,6 +41,7 @@ function Reservation() {
             <p>{DatePicker}</p>
             {/* <p>You typed: {inputValue}</p> */}
           </div>
+          
         </div>
       </div>
     </>
